1c050b6131dd5a9ef4bcec167b721d9b13c6ccce
mosclov <federico@grupofemal.com>
2019-04-01 12:52:07 -0700

    changes to devise to work on calendar

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,15 +6,20 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: [:google_oauth2]
   validates_format_of :email, with: /\@casinodemexicali\.com/, message: 'You should use your Casino email to sign in'
 
-   def self.from_omniauth(access_token)
-     data = access_token.info
+   def self.find_for_google_oauth2(auth)
+     data = auth.info
      user = User.where(email: data['email']).first
      unless user
        user = User.create(name: data['name'],
            email: data['email'],
-           password: Devise.friendly_token[0, 20]
+           password: Devise.friendly_token[0, 20],
+           provider: auth.provider,
+           uid: auth.uid
         )
      end
-     user
+     user.token = auth.credentials.token
+     user.refresh_token = auth.credentials.refresh_token
+     user.save
+     return user
    end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -297,6 +297,11 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
   Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], verify_iss: false
+    provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
+    access_type: "offline",
+    prompt: "consent",
+    select_account: true,
+    scope: 'userinfo.email,calendar',
+    verify_iss: false
   end
 end
